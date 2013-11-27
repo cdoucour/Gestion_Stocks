4f0c5db6753ab3aa64e7f865830656e444dc9e3e
amthiam <thiam36@gmail.com>
2013-11-27 09:26:47 +0100

    Ajout Sélection
    Gestion des stocks avec la sélection (au clic) qui marche. J'ai dû
    modifier certaines choses qui avaient été faites notamment dans la
    gestion de l'arbre.
    
    Pour l'instant après avoir sélectionné l'élément je me contente
    d'afficher son nom.
    
    J'ai aussi rajouté un panneau en bas pour afficher à l'utilisateur les
    infos sur l'élément qu'il a sélectionner
    A faire:
    -Rajouter le code de Clara pour avoir les boutons
    -Implémenter le code derrière les boutons
    -Faire le code du panneau du bas (afficher des infos sur l'élément
    sélectionné)

---
+++ Logiciel de gestion de stocks/Ajout_Selection/TreeDemo.java/src/TreeDemo/Product.java
@@ -0,0 +1,79 @@
+package TreeDemo;
+
+import TreeDemo.Category;
+import java.util.Date;
+import java.util.Calendar;
+import java.util.HashMap;
+
+
+public class Product extends Category{
+	Double price;
+	HashMap<Date,Double> quantityLevels;
+	Double currentQuantity;
+
+	public Product(String n) {
+		super(n);
+		quantityLevels = new HashMap<Date,Double>();
+	}
+	
+	public Product(String n, Double p) {
+		super(n);
+		this.price = p;
+		quantityLevels = new HashMap<Date,Double>();
+	}
+	
+	public Product(String n, Double p, Double currentQ) {
+		super(n);
+		this.price = p;
+		this.currentQuantity = currentQ;
+		quantityLevels = new HashMap<Date,Double>();
+		quantityLevels.put(Calendar.getInstance().getTime(), currentQ);
+	}
+	
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	/**
+	 * @return the quantityLevels
+	 */
+	public HashMap<Date, Double> getQuantityLevels() {
+		return quantityLevels;
+	}
+
+	/**
+	 * @param quantityLevels the quantityLevels to set
+	 */
+	public void setQuantityLevels(HashMap<Date, Double> quantityLevels) {
+		this.quantityLevels = quantityLevels;
+	}
+
+	
+
+	public Double getCurrentQuantity() {
+		return currentQuantity;
+	}
+
+	public double getQuantityLevelOnDate(Date d) {
+		System.out.println("Liste des historiques pour le produit" + this.getName());
+		for(Date date:quantityLevels.keySet()){
+			System.out.println(date.getTime());
+			System.out.println(d.getTime());
+		}
+		try{
+			return quantityLevels.get(d);
+		}
+		catch(Exception e){
+			System.out.println("Erreur:" + e.getMessage());
+			if(!quantityLevels.containsKey(d)){
+				System.out.println("La date " + d + " n'est pas dans l'historique...");
+			}
+			return 0;
+		}
+	}
+	
+}
